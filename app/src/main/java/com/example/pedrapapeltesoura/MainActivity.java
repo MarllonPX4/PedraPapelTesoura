d2407948bf1949c30c62adb3c90119f0ea086a21
MarllonPX4 <marllonprogramming@gmail.com>
2024-01-29 15:19:31 -0300

    Button/Function

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selecionarPedra(View view){
+        verificarGanhador("pedra");
+
+    }public void selecionarPapel(View view){
+        verificarGanhador("papel");
+
+    }public void selecionarTesoura(View view){
+        verificarGanhador("tesoura");
+
+    }
+    private void verificarGanhador(String escolhaUsuario){
+        System.out.println("Item clicado: " + escolhaUsuario);
+
+    }
+
 }
