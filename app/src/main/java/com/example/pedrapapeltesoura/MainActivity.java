ceb58a560e179bf6896a7c135160a7edaf1561e2
MarllonPX4 <marllonprogramming@gmail.com>
2024-01-30 20:20:12 -0300

    countPts

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.pedrapapeltesoura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.OptionalInt;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int ptsUserValue = 0;
+    int ptsAppValue = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,20 +51,31 @@ public class MainActivity extends AppCompatActivity {
         }
         return escolhaApp;
     }
+
+    @SuppressLint("ResourceAsColor")
     private void verificarGanhador(String escolhaUsuario){
 
         String escolhaApp = gerarEscolhaAleatoriaApp();
         TextView textResult = findViewById(R.id.textResult);
+        TextView ptsUser = findViewById(R.id.PtsUser);
+        TextView ptsApp = findViewById(R.id.PtsApp);
 
         if((escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
                 (escolhaApp == "papel" && escolhaUsuario == "pedra") ||
-                (escolhaApp == "tesoura" && escolhaUsuario == "papel") ){
+                (escolhaApp == "tesoura" && escolhaUsuario == "papel") ) {
             textResult.setText("Você Perdeu :(");
 
+            ptsUserValue = ptsUserValue + 1;
+            ptsUser.setText(Integer.toString(ptsUserValue));
+            ptsUser.setTextColor(Color.GREEN);
         }else if((escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
                 (escolhaUsuario == "papel" && escolhaApp == "pedra") ||
                 (escolhaUsuario == "tesoura" && escolhaApp == "papel") ){
-            textResult.setText("Você Ganhou :)");
+            textResult.setText("Você Ganhou ^^");
+
+            ptsAppValue = ptsAppValue + 1;
+            ptsApp.setText(Integer.toString(ptsAppValue));
+            ptsApp.setTextColor(Color.RED);
         }else{
             textResult.setText("Empate");
         }
